a8b6b08595426c69247868d79cb4774806fd2e84
ava.luxe <ajrluxe@gmail.com>
2023-10-29 00:40:12 -0500

    entering error handlers by middleware

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,8 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+const { ValidationError } = require('sequelize');
+
 
 const { environment } = require('./config');
 const isProduction = environment === 'production';
@@ -43,6 +45,50 @@ app.use(
 
 
 app.use(routes); // connects all the routes, needs to pass thru all the middleware, so goes at the bottom of all the middleware
+
+
+
+
+app.use((_req, _res, next) => {
+	const err = new Error("The requested resource couldn't be found.");
+	err.title = "Resource Not Found";
+	err.errors = ["The requested resource could not be found."];
+	err.status = 404;
+	next(err);
+}); // if a req gets to this point we make new obj and give it a new title where we catch it and process thru this middleware
+
+app.use((err, _req, _res, next) => {
+	// check if error is a Sequelize error:
+	if (err instanceof ValidationError) {
+		let errors = {};
+		for (let error of err.errors) {
+			errors[error.path] = error.message;
+		}
+		err.title = 'Validation error';
+		err.errors = errors;
+	}
+	next(err);
+});
+
+
+app.use((err, _req, res, _next) => {
+	res.status(err.status || 500);
+	console.error(err);
+	res.json({
+		title: err.title || 'Server Error',
+		message: err.message,
+		errors: err.errors,
+		stack: isProduction ? null : err.stack // stack in response IF were not in production
+	});
+});
+
+
+
+
+
+
+
+
 
 
 
