9d52143bb1ed3f481584e19759eef7c1b0cdbe87
ava.luxe <ajrluxe@gmail.com>
2023-10-29 17:25:33 -0500

    all tables and relative info W/O FULL ASSOCIATIONS complete

---
+++ backend/db/seeders/20231029210451-demo-spot-image.js
@@ -0,0 +1,141 @@
+'use strict';
+
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('SpotImages', [
+      {
+        spotId: 1,
+        url: 'https://i.imgur.com/OJkgHbF.png',
+        preview: true
+      },
+      {
+        spotId: 1,
+        url: 'https://i.imgur.com/JI7YHkY.png',
+        preview: true
+      },
+      {
+        spotId: 1,
+        url: 'https://i.imgur.com/A88bfyT.png',
+        preview: true
+      },
+      {
+        spotId: 1,
+        url: 'https://i.imgur.com/XXe7q.png',
+        preview: true
+      },
+      {
+        spotId: 1,
+        url: 'https://i.imgur.com/Fol9IbU.png',
+        preview: true
+      },
+      {
+        spotId: 2,
+        url: 'https://i.imgur.com/HCjamvj.png',
+        preview: true
+      },
+      {
+        spotId: 2,
+        url: 'https://i.imgur.com/NJs94rn.png',
+        preview: true
+      },
+      {
+        spotId: 2,
+        url: 'https://i.imgur.com/8rpGibn.png',
+        preview: true
+      },
+      {
+        spotId: 2,
+        url: 'https://i.imgur.com/nEboj.png',
+        preview: true
+      },
+      {
+        spotId: 2,
+        url: 'https://i.imgur.com/bUc5CnG.png',
+        preview: true
+      },
+      {
+        spotId: 3,
+        url: 'https://i.imgur.com/D2StPP2.png',
+        preview: true
+      },
+      {
+        spotId: 3,
+        url: 'https://i.imgur.com/NCa79sE.png',
+        preview: true
+      },
+      {
+        spotId: 3,
+        url: 'https://i.imgur.com/y3QIHSh.png',
+        preview: true
+      },
+      {
+        spotId: 3,
+        url: 'https://i.imgur.com/R5tfvOP.png',
+        preview: true
+      },
+      {
+        spotId: 3,
+        url: 'https://i.imgur.com/fUIPgLY.png',
+        preview: true
+      },
+      {
+        spotId: 4,
+        url: 'https://i.imgur.com/N5DIm78.png',
+        preview: true
+      },
+      {
+        spotId: 4,
+        url: 'https://i.imgur.com/Osc3728.png',
+        preview: true
+      },
+      {
+        spotId: 4,
+        url: 'https://i.imgur.com/q70guJ9.png',
+        preview: true
+      },
+      {
+        spotId: 4,
+        url: 'https://i.imgur.com/F93jFut.png',
+        preview: true
+      },
+      {
+        spotId: 4,
+        url: 'https://i.imgur.com/nxOngak.png',
+        preview: true
+      },
+      {
+        spotId: 5,
+        url: 'https://i.imgur.com/GiMfAr3.png',
+        preview: true
+      },
+      {
+        spotId: 5,
+        url: 'https://i.imgur.com/SrydAAY.png',
+        preview: true
+      },
+      {
+        spotId: 5,
+        url: 'https://i.imgur.com/X7dHrAN.png',
+        preview: true
+      },
+      {
+        spotId: 5,
+        url: 'https://i.imgur.com/6n4ngcq.png',
+        preview: true
+      },
+      {
+        spotId: 5,
+        url: 'https://i.imgur.com/Ycuuk2G.png',
+        preview: true
+      }
+    ])
+  },
+
+  async down (queryInterface, Sequelize) {
+
+    return queryInterface.bulkDelete('SpotImages', {
+      spotId: { [Op.in]: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15] }
+    }, {});
+  }
+};
