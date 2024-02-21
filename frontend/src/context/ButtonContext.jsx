324b421ab13dfc31211119851a81e08ece100d70
ava.luxe <ajrluxe@gmail.com>
2024-02-21 17:01:01 -0600

    adding final touches and debugging to profilebutton and menubutton components to share buttoncontext

--- frontend/src/context/ButtonContext.jsx
+++ frontend/src/context/ButtonContext.jsx
@@ -1,30 +1,29 @@
-import { createContext, useState, useEffect } from "react";
-
+import { createContext, useState, useEffect, useRef } from "react";
 
 export const ButtonContext = createContext();
 
-export function ButtonProvider() { //* add user here?
+export function ButtonProvider(props) {
 
 	// dup profile button code, extrapolate what's necessary for sharing
 	const ulRef = useRef();
 	const [showMenu, setShowMenu] = useState(false);
 	const [hover, setHover] = useState(false);
 
+	// const dispatch = useDispatch();
+
 	// const onHover = () => {
 	// 	setHover(true)
 	// };
 
-	// const offHover = () => {
-	// 	setHover(!hover)
-	// }
+
 
 	// const hovering = (e) => {
 	// 	e.stopPropagation();
 
-	// 	offHover();
+	// 	setHover(!hover)
 	// }
 
-	const toggleMenu = (e) => {
+	const toggleMenuButton = (e) => {
 		e.stopPropagation();// Keep click from bubbling up to document and triggering closeMenu
 		setShowMenu(!showMenu)
 	}
@@ -47,14 +46,14 @@ export function ButtonProvider() { //* add user here?
 
 	const closeMenu = () => setShowMenu(false);
 
-	const logout = (e) => {
-		e.preventDefault();
+	// const logout = (e) => {
+	// 	e.preventDefault();
 
-		dispatch(sessionActions.logout());
-		closeMenu();
-		navigate('/')
-		alert("You have logged out.")
-	}
+	// 	dispatch(sessionActions.logout());
+	// 	closeMenu();
+	// 	navigate('/')
+	// 	alert("You have logged out.")
+	// }
 
 	// const manageSpots = (e) => {
 	// 	e.preventDefault();
@@ -65,7 +64,7 @@ export function ButtonProvider() { //* add user here?
 	// }
 
 	return (
-		<ButtonContext.Provider value={{ showMenu, setShowMenu, hover, setHover, toggleMenu, logout}}>
+		<ButtonContext.Provider value={{ showMenu, setShowMenu, hover, setHover, closeMenu, toggleMenuButton, ulRef }}>
 			{props.children}
 		</ButtonContext.Provider>
 	)
